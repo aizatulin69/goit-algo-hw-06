80eb09aa0e18b63d37e404c45ce9a02f3544556b
aizatulin69 <kirilajzatulin@gmail.com>
2025-08-13 03:56:57 +0300

    Update 6hw.py

--- 6hw.py
+++ 6hw.py
@@ -1,7 +1,5 @@
-
 from collections import UserDict
 
-contacts = {}
 
 class Field:
     def __init__(self, value):
@@ -12,15 +10,13 @@ class Field:
 
 
 class Name(Field):
-    # Я не зрозумів, як можна реалізувати цей класс
-    # Сподіваюсь, що ви уточните 
     pass
-        
+
 
 class Phone(Field):
     def __init__(self, value):
         if not (value.isdigit() and len(value) == 10):
-            raise ValueError("Номер телефона должен содержать ровно 10 цифр")
+            raise ValueError
         super().__init__(value)
 
 
@@ -30,24 +26,20 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone):
-        if phone in self.phones:
-            return "Phone already exists"
         self.phones.append(Phone(phone))
-        return f"Phone {phone} added for {self.name.value}."
 
     def edit_phone(self, old_phone, new_phone):
-        for i, p in enumerate(self.phones):
-            if p.value == old_phone:
-                self.phones[i] = Phone(new_phone)
-                return f"Phone {old_phone} replaced by {new_phone}."
-        return "Phone not found."
+        phone = self.find_phone(old_phone)
+        if phone is not None:
+            self.remove_phone(phone.value)
+            self.add_phone(new_phone)
 
     def remove_phone(self, phone):
-        for p in self.phones:
-            if p.value == phone:
-                self.phones.remove(p)
-                return f"Phone {phone} is deleted."
-        return "Phone not found."
+        p = self.find_phone(phone)
+        if p:
+            self.phones.remove(p)
+            return p
+        return None
 
     def find_phone(self, phone):
         for p in self.phones:
@@ -69,8 +61,6 @@ class AddressBook(UserDict):
     def delete(self, name):
         if name in self.data:
             del self.data[name]
-            return f"Contact {name} removed."
-        return "Contact not found."
-    
+
     def __str__(self):
         return "\n".join(str(record) for record in self.data.values())
